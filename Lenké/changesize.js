6fe1c1df258a5cb474c52a72f23b07ba2d2baf77
Juan José García <39605602+JuanJo193@users.noreply.github.com>
2021-11-29 13:03:16 -0500

    Lenké Desing prueba1

---
+++ Lenké/changesize.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$(".cambiosize").click(function redimensionarDiv(){
+    var paddingBottom = '75vh'
+    $(".map-container-4").css({"padding-bottom": paddingBottom});
+    
+});
+$(".cambiosize2").click(function redimensionarDiv(){
+    var paddingBottom = '100vh'
+    $(".map-container-4").css({"padding-bottom": paddingBottom});
+    
+});
+});
